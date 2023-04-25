19eff15fee0ef118f91afe15099e6ae3150f7377
VioVayo <127029039+VioVayo@users.noreply.github.com>
2023-04-25 23:32:30 +0200

    Item-specific physical collision

--- YeetMod/ItemYeetSocket.cs
+++ YeetMod/ItemYeetSocket.cs
@@ -8,7 +8,8 @@ namespace YeetMod
         private float initialVelocity;
 
         private OWRigidbody owRigidbody;
-        private bool wasOnInteractible;
+        public GameObject detectorObj = new("YeetDetector");
+        private bool switchedFromInteractible;
 
 
         public static ItemYeetSocket Create(OWItem item, Vector3 startingPosition, float startingVelocity)
@@ -25,17 +26,17 @@ namespace YeetMod
 
         private void Awake()
         {
-            gameObject.layer = LayerMask.NameToLayer("AdvancedDetector");
+            detectorObj.layer = LayerMask.NameToLayer("PhysicalDetector");
+            gameObject.layer = LayerMask.NameToLayer("PhysicalDetector");
             gameObject.tag = "DynamicPropDetector";
 
+            detectorObj.transform.SetParent(transform, false);
             owRigidbody = gameObject.AddComponent<OWRigidbody>();
-            gameObject.AddComponent<SphereCollider>();
-            gameObject.AddComponent<SphereShape>()._collisionMode = Shape.CollisionMode.Detector;
-            gameObject.AddComponent<DynamicForceDetector>();
-            gameObject.AddComponent<DynamicFluidDetector>();
+            detectorObj.AddComponent<DynamicForceDetector>();
+            detectorObj.AddComponent<DynamicFluidDetector>();
+            AddShapesAndColliders();
+            detectorObj.transform.SetParent(attachedItem.transform, false);
 
-            wasOnInteractible = attachedItem.gameObject.layer == LayerMask.NameToLayer("Interactible");
-            if (wasOnInteractible) attachedItem.gameObject.layer = LayerMask.NameToLayer("Default");
             attachedItem.onPickedUp += OnPickUpItem;
 
             foreach (var volume in Locator.GetPlayerBody()._attachedForceDetector._activeVolumes) owRigidbody._attachedForceDetector.AddVolume(volume);
@@ -48,11 +49,128 @@ namespace YeetMod
             owRigidbody.SetVelocity(Locator.GetPlayerBody().GetPointVelocity(transform.position) + Locator.GetPlayerCamera().transform.forward * initialVelocity);
         }
 
+        private void AddShapesAndColliders()
+        {
+            switch (attachedItem._type)
+            {
+                case ItemType.Scroll:
+                    var scrollCollider = detectorObj.AddComponent<CapsuleCollider>();
+                    var scrollShape = detectorObj.AddComponent<CapsuleShape>();
+                    scrollCollider.height = 0.8f;
+                    scrollCollider.radius = 0.2f;
+                    scrollShape.CopySettingsFromCollider();
+                    scrollShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    OrientDetector(new Vector3(0, 0, -0.37f), Quaternion.Euler(90, 0, 0));
+                    break;
+                case ItemType.WarpCore:
+                    var warpCore = attachedItem as WarpCoreItem;
+                    if (warpCore._warpCoreType is WarpCoreType.Vessel or WarpCoreType.VesselBroken)
+                    {
+                        var awcCollider = detectorObj.AddComponent<BoxCollider>();
+                        var awcShape = detectorObj.AddComponent<BoxShape>();
+                        awcCollider.size = new Vector3(0.35f, 1.15f, 0.35f);
+                        awcShape.CopySettingsFromCollider();
+                        awcShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                        OrientDetector(Vector3.zero, Quaternion.Euler(0, 45, 0));
+                    }
+                    else
+                    {
+                        var wcCollider = detectorObj.AddComponent<BoxCollider>();
+                        var wcShape = detectorObj.AddComponent<BoxShape>();
+                        wcCollider.size = new Vector3(0.4f, 0.1f, 0.4f);
+                        wcShape.CopySettingsFromCollider();
+                        wcShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                        OrientDetector(new Vector3(0, 10.04f, 0), Quaternion.Euler(45, 0, 90));
+                    }
+                    break;
+                case ItemType.SharedStone:
+                    var projStoneCollider = detectorObj.AddComponent<BoxCollider>();
+                    var projStoneShape = detectorObj.AddComponent<BoxShape>();
+                    projStoneCollider.size = new Vector3(0.4f, 0.1f, 0.4f);
+                    projStoneShape.CopySettingsFromCollider();
+                    projStoneShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    break;
+                case ItemType.ConversationStone:
+                    var convStoneCollider = detectorObj.AddComponent<SphereCollider>();
+                    var convStoneShape = detectorObj.AddComponent<SphereShape>();
+                    convStoneCollider.radius = 0.24f;
+                    convStoneShape.CopySettingsFromCollider();
+                    convStoneShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    break;
+                case ItemType.Lantern:
+                    var lanternCapsuleCollider = detectorObj.AddComponent<CapsuleCollider>();
+                    var lanternCapsuleShape = detectorObj.AddComponent<CapsuleShape>();
+                    var lanternBoxCollider = detectorObj.AddComponent<BoxCollider>();
+                    var lanternBoxShape = detectorObj.AddComponent<BoxShape>();
+                    lanternCapsuleCollider.center = new Vector3(0, 0.35f, 0);
+                    lanternCapsuleCollider.height = 0.4f;
+                    lanternCapsuleCollider.radius = 0.2f;
+                    lanternBoxCollider.size = new Vector3(0.25f, 0.1f, 0.25f);
+                    lanternCapsuleShape.CopySettingsFromCollider();
+                    lanternCapsuleShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    lanternBoxShape.CopySettingsFromCollider();
+                    lanternBoxShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    break;
+                case ItemType.SlideReel:
+                    var reelCollider = detectorObj.AddComponent<CapsuleCollider>();
+                    var reelShape = detectorObj.AddComponent<CapsuleShape>();
+                    reelCollider.center = new Vector3(0, 0.2f, 0);
+                    reelCollider.height = 0.37f;
+                    reelCollider.radius = 0.4f;
+                    reelShape.CopySettingsFromCollider();
+                    reelShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    break;
+                case ItemType.DreamLantern:
+                    var dreamLanternCapsuleCollider = detectorObj.AddComponent<CapsuleCollider>();
+                    var dreamLanternCapsuleShape = detectorObj.AddComponent<CapsuleShape>();
+                    var dreamLanternBoxCollider = detectorObj.AddComponent<BoxCollider>();
+                    var dreamLanternBoxShape = detectorObj.AddComponent<BoxShape>();
+                    dreamLanternCapsuleCollider.center = new Vector3(0.35f, 0, 0);
+                    dreamLanternCapsuleCollider.height = 0.4f;
+                    dreamLanternCapsuleCollider.radius = 0.37f;
+                    dreamLanternBoxCollider.size = new Vector3(0.15f, 0.37f, 0.37f);
+                    dreamLanternCapsuleShape.CopySettingsFromCollider();
+                    dreamLanternCapsuleShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    dreamLanternBoxShape.CopySettingsFromCollider();
+                    dreamLanternBoxShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    OrientDetector(Vector3.zero, Quaternion.Euler(0, 0, 90));
+                    break;
+                case ItemType.VisionTorch:
+                    var torchCapsuleCollider = detectorObj.AddComponent<CapsuleCollider>();
+                    var torchCapsuleShape = detectorObj.AddComponent<CapsuleShape>();
+                    var torchSphereCollider = detectorObj.AddComponent<SphereCollider>();
+                    var torchSphereShape = detectorObj.AddComponent<SphereShape>();
+                    torchCapsuleCollider.height = 1.8f;
+                    torchCapsuleCollider.radius = 0.1f;
+                    torchSphereCollider.center = new Vector3(0, 0.9f, 0.05f);
+                    torchSphereCollider.radius = 0.4f;
+                    torchCapsuleShape.CopySettingsFromCollider();
+                    torchCapsuleShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    torchSphereShape.CopySettingsFromCollider();
+                    torchSphereShape.SetCollisionMode(Shape.CollisionMode.Detector);
+                    break;
+                default:
+                    detectorObj.layer = LayerMask.NameToLayer("AdvancedDetector");
+                    switchedFromInteractible = attachedItem.gameObject.layer == LayerMask.NameToLayer("Interactible");
+                    if (switchedFromInteractible) attachedItem.gameObject.layer = LayerMask.NameToLayer("Default");
+                    detectorObj.AddComponent<SphereCollider>();
+                    detectorObj.AddComponent<SphereShape>().SetCollisionMode(Shape.CollisionMode.Detector);
+                    break;
+            }
+        }
+
+        private void OrientDetector(Vector3 localPos, Quaternion localRot)
+        {
+            detectorObj.transform.localPosition = localPos;
+            detectorObj.transform.localRotation = localRot;
+        }
+
         private void OnPickUpItem(OWItem item)
         {
             attachedItem.onPickedUp -= OnPickUpItem;
-            if (wasOnInteractible) attachedItem.gameObject.layer = LayerMask.NameToLayer("Interactible");
+            if (switchedFromInteractible) attachedItem.gameObject.layer = LayerMask.NameToLayer("Interactible");
             Destroy(gameObject);
+            Destroy(detectorObj);
         }
     }
 }
